acbc2b95524790a8ea781ca4a97a5fc5058b42d4
frans <fransoriano37@live.com>
2023-12-08 21:34:04 +0100

    metodo editar, eliminar y añadir actvidiad terminado. falta mostrar las horas realizadas y restantes de alumno

--- src/main/java/com/example/proyectogrupal/controllers/EditarActividadDeAlumno.java
+++ src/main/java/com/example/proyectogrupal/controllers/EditarActividadDeAlumno.java
@@ -2,6 +2,8 @@ package com.example.proyectogrupal.controllers;
 
 import com.example.proyectogrupal.App;
 import com.example.proyectogrupal.Session;
+import com.example.proyectogrupal.actividad.Actividad;
+import com.example.proyectogrupal.actividad.ActividadDAO;
 import com.example.proyectogrupal.actividad.Tips;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
@@ -11,6 +13,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class EditarActividadDeAlumno implements Initializable {
@@ -28,13 +31,15 @@ public class EditarActividadDeAlumno implements Initializable {
     @javafx.fxml.FXML
     private TextField txtObservaciones;
     @javafx.fxml.FXML
-    private Button btnCancelar;
-    @javafx.fxml.FXML
     private Label labelNombre;
     @javafx.fxml.FXML
     private Button btnActualizar;
 
     private final ToggleGroup toggleGroup = new ToggleGroup();
+    @javafx.fxml.FXML
+    private Button btnEliminar;
+    @javafx.fxml.FXML
+    private Button btnCancelar1;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -43,7 +48,7 @@ public class EditarActividadDeAlumno implements Initializable {
         txtActividad.setText(Session.getCurrentActividad().getActividad());
         txtObservaciones.setText(Session.getCurrentActividad().getObservaciones());
 
-        spinnerHoras.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(Math.toIntExact(Session.getCurrentActividad().getHoras()), 8,  1));
+        spinnerHoras.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 8, Math.toIntExact(Session.getCurrentActividad().getHoras()), 1));
 
         //Esto es para poder pasar un DataPicker a otro sino no deja pasar los datos
         LocalDate actfecha = Session.getCurrentActividad().getFecha();
@@ -53,11 +58,20 @@ public class EditarActividadDeAlumno implements Initializable {
         rbFCT.setToggleGroup(toggleGroup);
 
         var tipo = Session.getCurrentActividad().getTipo();
-        toggleGroup.selectedToggleProperty(tipo);
 
-
+        if (tipo != null) {
+            switch (tipo) {
+                case DUAL:
+                    toggleGroup.selectToggle(rbDual);
+                    break;
+                case FCT:
+                    toggleGroup.selectToggle(rbFCT);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
-
 
 
     @javafx.fxml.FXML
@@ -71,7 +85,79 @@ public class EditarActividadDeAlumno implements Initializable {
 
 
     @javafx.fxml.FXML
-    public void btnActualizar(ActionEvent actionEvent) {
+    public void btnActualizar(ActionEvent actionEvent) throws IOException {
+        // Obtener la instancia de la actividad actual
+        Actividad actividad = Session.getCurrentActividad();
 
+        // Actualizar las propiedades con los valores de los controles de la interfaz de usuario
+        if (txtActividad.getText().length() > 1) {
+            actividad.setActividad(txtActividad.getText());
+        }
+        if (txtObservaciones.getText().length() > 1) {
+            actividad.setObservaciones(txtObservaciones.getText());
+        }
+
+        actividad.setHoras((Integer) spinnerHoras.getValue());
+
+        // Obtener la fecha seleccionada
+        LocalDate fechaSeleccionada = DatePickerFecha.getValue();
+        if (fechaSeleccionada != null) {
+            actividad.setFecha(fechaSeleccionada);
+        } else {
+            System.out.println("Error: Debes seleccionar una fecha.");
+            return;
+        }
+
+        // Obtener el tipo de práctica
+        RadioButton seleccion = (RadioButton) toggleGroup.getSelectedToggle();
+        if (seleccion != null) {
+            if (seleccion == rbDual) {
+                actividad.setTipo(Tips.DUAL);
+            } else if (seleccion == rbFCT) {
+                actividad.setTipo(Tips.FCT);
+            }
+        }
+
+        ActividadDAO actividadDAO = new ActividadDAO();
+
+        if (actividad.getID_Actividad() != null){
+            actividadDAO.update(actividad);
+        } else {
+            actividadDAO.save(actividad);
+        }
+
+        try {
+            App.changeScene("PaginaAlumno.fxml", "Pagina Alumno");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @javafx.fxml.FXML
+    public void eliminar(ActionEvent actionEvent) throws IOException {
+        // Obtener la actividad actual seleccionada
+        Actividad actividadSeleccionada = Session.getCurrentActividad();
+        ActividadDAO actividadDAO = new ActividadDAO();
+
+        // Crear un cuadro de diálogo de confirmación
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Eliminar Actividad");
+        alert.setHeaderText(null);
+        alert.setContentText("¿Estás seguro de que deseas eliminar esta actividad?");
+
+        // Mostrar el cuadro de diálogo y esperar la respuesta del usuario
+        Optional<ButtonType> result = alert.showAndWait();
+
+        // Verificar la respuesta del usuario
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+
+            actividadDAO.delete(actividadSeleccionada);
+        }
+        try{
+            App.changeScene("PaginaAlumno.fxml", "Pagina Alumno");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+
